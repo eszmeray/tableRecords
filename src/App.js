8bff4afa6bb97fbf9501e73710ed39f6f452c4bd
Amor Padua <amoreepadua@gmail.com>
2024-08-22 00:40:53 +0800

    changes 4

--- src/App.js
+++ src/App.js
@@ -1,4 +1,25 @@
+//tblHatcheryRecords
 import './App.css';
+import {Table} from "./tables/tblHatcheryRecords/tblHatcheryRecords";
+import {Buttons} from "./tables/tblHatcheryRecords/Buttons";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Buttons/>
+      <Table />
+    </div>
+  );
+}
+
+
+export default App;
+
+
+//tblUserRecords
+
+/*import './App.css';
 import {Table} from "./tables/tblUserRecords/tblUserRecords";
 import {Buttons} from "./tables/tblUserRecords/Buttons";
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -13,4 +34,4 @@ function App() {
 }
 
 
-export default App;
+export default App;*/
