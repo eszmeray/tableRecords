6617a6a40eb2825cba1bf2a55884ee832b5071f6
Amor Padua <amoreepadua@gmail.com>
2024-09-14 23:35:33 +0800

    filter draft user

--- src/tables/tblUserRecords/Buttons.jsx
+++ src/tables/tblUserRecords/Buttons.jsx
@@ -1,13 +1,17 @@
 import React, { useState, useRef, useEffect, useCallback } from 'react';
 import './styles/tblUserRecords.css';
 import { IoIosFunnel, IoIosAdd } from "react-icons/io";
-import { FaSearch } from 'react-icons/fa';
+import { FaSearch, FaSlidersH } from 'react-icons/fa';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 export const Buttons = () => {
   const [filterCriteria, setFilterCriteria] = useState('');
+  const [dropdownOpen, setDropdownOpen] = useState(false);
   const [showModal, setShowModal] = useState(false);
+  const [applyMessage, setApplyMessage] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
+  const modalRef = useRef(null);
   const [formData, setFormData] = useState({
     firstName: '',
     middleName: '',
@@ -22,12 +26,30 @@ export const Buttons = () => {
     role: '',
     email: '',
   });
-  const [successMessage, setSuccessMessage] = useState('');
-  const modalRef = useRef(null);
 
   const handleFilterChange = (criteria) => {
     setFilterCriteria(criteria);
   };
+
+
+  const handleReset = () => {
+    setFilterCriteria('');
+    setFormData({
+      firstName: '',
+      lastName: '',
+
+    });
+
+
+    setFormErrors({
+      firstName: '',
+      lastName: '',
+
+    });
+
+    console.log('Filters have been reset');
+  };
+
 
   const handleSearchChange = (event) => {
     const searchValue = event.target.value;
@@ -100,14 +122,32 @@ export const Buttons = () => {
     return Object.keys(errors).length === 0;
   };
 
+  const validateDropdownFilters = () => {
+    let isValid = true;
+
+    return isValid;
+  };
+
+
+
   const handleAddClick = () => {
     if (validateForm()) {
       console.log('Form Data:', formData);
       setSuccessMessage('Data has been added successfully.');
       toggleModal();
-      setTimeout(() => setSuccessMessage(''), 3000); 
+      setTimeout(() => setSuccessMessage(''), 3000);
     }
   };
+
+  const handleApplyClick = () => {
+    if (validateDropdownFilters()) {
+      setApplyMessage('Filters have been applied successfully.');
+      setTimeout(() => setApplyMessage(''), 3000);
+      handleReset();
+    }
+  };
+
+
 
   return (
     <div className="container my-12">
@@ -120,7 +160,7 @@ export const Buttons = () => {
             type="text"
             className="form-control custom-search"
             placeholder="Search"
-            onChange={handleSearchChange} 
+            onChange={handleSearchChange}
           />
           <button className='search-button' onClick={handleSearchClick}>
             <FaSearch className='search-icon' />
@@ -128,115 +168,167 @@ export const Buttons = () => {
         </div>
         <div className='btn-group'>
           <div className="dropdown">
-            <button className="btn btn-filter dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Filter by type <IoIosFunnel className='funnel-icon' />
+            <button className="btn btn-filter dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded={dropdownOpen}>
+              <FaSlidersH className='funnel-icon' /> Sorting Options
             </button>
-            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <a className="dropdown-item" href="#" onClick={() => handleFilterChange('firstName')}>First Name</a>
-              <a className="dropdown-item" href="#" onClick={() => handleFilterChange('lastName')}>Last Name</a>
-              <a className="dropdown-item" href="#" onClick={() => handleFilterChange('role')}>Role</a>
-              <a className="dropdown-item" href="#" onClick={() => handleFilterChange('password')}>Password</a>
-              <a className="dropdown-item" href="#" onClick={() => handleFilterChange('email')}>Email</a>
+            <div className={`dropdown-menu p-3 ${dropdownOpen ? 'show' : ''}`} aria-labelledby="dropdownMenuButton">
+              <h5 className='filterTitle'>Sort by Category</h5>
+
+
+
+              <div className="dropdown-item">
+                <div className="row">
+                  <div className="col-md-6">
+                    <h6 className="filterSubtitle">First Name</h6>
+                    <hr></hr>
+                    <div className="form-check">
+                      <input className="form-check-input" type="radio" name="flexRadioDefaultFirstName" id="flexRadioDefault1" />
+                      <label className="form-check-label" htmlFor="flexRadioDefault1">
+                        Ascending
+                      </label>
+                    </div>
+                    <div className="form-check">
+                      <input className="form-check-input" type="radio" name="flexRadioDefaultFirstName" id="flexRadioDefault2" />
+                      <label className="form-check-label" htmlFor="flexRadioDefault2">
+                        Descending
+                      </label>
+                    </div>
+                  </div>
+
+                  <div className="col-md-6">
+                    <h6 className="filterSubtitle">Last Name</h6>
+                    <hr></hr>
+
+                    <div className="form-check">
+                      <input className="form-check-input" type="radio" name="flexRadioDefaultLastName" id="flexRadioDefault3" />
+                      <label className="form-check-label" htmlFor="flexRadioDefault3">
+                        Ascending
+                      </label>
+                    </div>
+                    <div className="form-check">
+                      <input className="form-check-input" type="radio" name="flexRadioDefaultLastName" id="flexRadioDefault4" />
+                      <label className="form-check-label" htmlFor="flexRadioDefault4">
+                        Descending
+                      </label>
+
+                      <br></br>
+
+                      <button className='btn btn-secondary cancel' onClick={handleReset}>CANCEL</button>
+                      <button className='btn btn-primary apply' onClick={handleApplyClick}>APPLY</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
           <button className="btn btn-primary" onClick={toggleModal}><IoIosAdd /> NEW RECORD</button>
-        </div>
-      </div>
 
+        </div>
+
+
+
+
+        {showModal && (
+          <>
+            <div className="modal-background-blur"></div>
+            <div className="modal fade show" style={{ display: 'block' }} tabIndex="-1">
+              <div className="modal-dialog modal-dialog-centered">
+                <div className="modal-content" ref={modalRef}>
+
+                  <div className="modal-body">
+                    <h2 className="modal-title">New Record</h2>
+                    <h6 className='modal-subtitle'>Who’s joining us next? Create their user profile</h6>
+                    <br></br>
+                    <form>
+                      <div className="form-group">
+                        <label htmlFor="firstName">First Name</label>
+                        <input
+                          type="text"
+                          className={`form-control ${formErrors.firstName ? 'is-invalid' : ''}`}
+                          id="firstName"
+                          placeholder="Enter first name"
+                          value={formData.firstName}
+                          onChange={handleInputChange}
+                        />
+                        {formErrors.firstName && <div className="invalid-feedback">{formErrors.firstName}</div>}
+                      </div>
+
+                      <div className="form-group">
+                        <label htmlFor="firstName">Middle Name</label>
+                        <input
+                          type="text"
+                          className={`form-control ${formErrors.middleName ? 'is-invalid' : ''}`}
+                          id="middleName"
+                          placeholder="Enter middle name"
+                          value={formData.middleName}
+                          onChange={handleInputChange}
+                        />
+                        {formErrors.middleName && <div className="invalid-feedback">{formErrors.middleName}</div>}
+                      </div>
+
+                      <div className="form-group">
+                        <label htmlFor="lastName">Last Name</label>
+                        <input
+                          type="text"
+                          className={`form-control ${formErrors.lastName ? 'is-invalid' : ''}`}
+                          id="lastName"
+                          placeholder="Enter last name"
+                          value={formData.lastName}
+                          onChange={handleInputChange}
+                        />
+                        {formErrors.lastName && <div className="invalid-feedback">{formErrors.lastName}</div>}
+                      </div>
+                      <div className="form-group">
+                        <label htmlFor="role">Role</label>
+                        <select
+                          className={`form-control ${formErrors.role ? 'is-invalid' : ''}`}
+                          id="role"
+                          value={formData.role}
+                          onChange={handleInputChange}
+                        >
+                          <option value="">Select role</option>
+                          <option value="admin">Admin</option>
+                          <option value="user">Bantay Dagat</option>
+                        </select>
+                        {formErrors.role && <div className="invalid-feedback">{formErrors.role}</div>}
+                      </div>
+
+                      <div className="form-group">
+                        <label htmlFor="email">Email</label>
+                        <input
+                          type="email"
+                          className={`form-control ${formErrors.email ? 'is-invalid' : ''}`}
+                          id="email"
+                          placeholder="Enter email"
+                          value={formData.email}
+                          onChange={handleInputChange}
+                        />
+                        {formErrors.email && <div className="invalid-feedback">{formErrors.email}</div>}
+                      </div>
+                    </form>
+                  </div>
+                  <div className="modal-footer">
+                    <button type="button" className="btn btn-secondary" onClick={toggleModal}>CANCEL</button>
+                    <button type="button" className="btn btn-primary" onClick={handleAddClick}>ADD</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </>
+        )}
+      </div>
       {successMessage && (
         <div className="success-message">
           {successMessage}
         </div>
       )}
 
-      {showModal && (
-        <>
-          <div className="modal-background-blur"></div>
-          <div className="modal fade show" style={{ display: 'block' }} tabIndex="-1">
-            <div className="modal-dialog modal-dialog-centered">
-              <div className="modal-content" ref={modalRef}>
-              
-                <div className="modal-body">
-                <h2 className="modal-title">New Record</h2>
-                <h6 className='modal-subtitle'>Who’s joining us next? Create their user profile</h6>
-                <br></br>
-                <form>
-                    <div className="form-group">
-                      <label htmlFor="firstName">First Name</label>
-                      <input
-                        type="text"
-                        className={`form-control ${formErrors.firstName ? 'is-invalid' : ''}`}
-                        id="firstName"
-                        placeholder="Enter first name"
-                        value={formData.firstName}
-                        onChange={handleInputChange}
-                      />
-                      {formErrors.firstName && <div className="invalid-feedback">{formErrors.firstName}</div>}
-                    </div>
-
-                    <div className="form-group">
-                      <label htmlFor="firstName">Middle Name</label>
-                      <input
-                        type="text"
-                        className={`form-control ${formErrors.middleName ? 'is-invalid' : ''}`}
-                        id="middleName"
-                        placeholder="Enter middle name"
-                        value={formData.middleName}
-                        onChange={handleInputChange}
-                      />
-                      {formErrors.middleName && <div className="invalid-feedback">{formErrors.middleName}</div>}
-                    </div>
-
-                    <div className="form-group">
-                      <label htmlFor="lastName">Last Name</label>
-                      <input
-                        type="text"
-                        className={`form-control ${formErrors.lastName ? 'is-invalid' : ''}`}
-                        id="lastName"
-                        placeholder="Enter last name"
-                        value={formData.lastName}
-                        onChange={handleInputChange}
-                      />
-                      {formErrors.lastName && <div className="invalid-feedback">{formErrors.lastName}</div>}
-                    </div>
-                    <div className="form-group">
-                      <label htmlFor="role">Role</label>
-                      <select
-                        className={`form-control ${formErrors.role ? 'is-invalid' : ''}`}
-                        id="role"
-                        value={formData.role}
-                        onChange={handleInputChange}
-                      >
-                        <option value="">Select role</option>
-                        <option value="admin">Admin</option>
-                        <option value="user">Bantay Dagat</option>
-                      </select>
-                      {formErrors.role && <div className="invalid-feedback">{formErrors.role}</div>}
-                    </div>
-                 
-                    <div className="form-group">
-                      <label htmlFor="email">Email</label>
-                      <input
-                        type="email"
-                        className={`form-control ${formErrors.email ? 'is-invalid' : ''}`}
-                        id="email"
-                        placeholder="Enter email"
-                        value={formData.email}
-                        onChange={handleInputChange}
-                      />
-                      {formErrors.email && <div className="invalid-feedback">{formErrors.email}</div>}
-                    </div>
-                  </form>
-                </div>
-                <div className="modal-footer">
-                  <button type="button" className="btn btn-secondary" onClick={toggleModal}>CANCEL</button>
-                  <button type="button" className="btn btn-primary" onClick={handleAddClick}>ADD</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </>
+      {applyMessage && (
+        <div className="apply-message">
+          {applyMessage}
+        </div>
       )}
     </div>
+
   );
 };
